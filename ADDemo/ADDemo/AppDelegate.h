2e9c0060a661038bc72ec349abb13e539aaa170c
Tony0822 <constyang@163.com>
2018-07-24 18:28:20 +0800

    创建工程

---
+++ ADDemo/ADDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADDemo
+//
+//  Created by TonyYang on 2018/7/24.
+//  Copyright © 2018年 TonyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADDemo/ADDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADDemo
+//
+//  Created by TonyYang on 2018/7/24.
+//  Copyright © 2018年 TonyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
